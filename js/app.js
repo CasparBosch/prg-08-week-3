2f11b6d08d60557f5e67735868483a24947e70a5
CasparBosch <1004288@hr.nl>
2023-02-24 14:18:21 +0100

    ML samenvoegen

--- js/app.js
+++ js/app.js
@@ -1,15 +1,23 @@
 const video = document.getElementById("webcam");
+const featureExtractor = ml5.featureExtractor('MobileNet', modelLoaded)
 const label = document.getElementById("label");
+let classifier
 
 const labelOneBtn = document.querySelector("#labelOne");
 const labelTwoBtn = document.querySelector("#labelTwo");
 const labelThreeBtn = document.querySelector("#labelThree");
 const trainbtn = document.querySelector("#train");
 
+const nomaskbtn = document.querySelector("#nomask")
+const maskbtn = document.querySelector("#mask")
+
 labelOneBtn.addEventListener("click", () => console.log("button 1"));
 labelTwoBtn.addEventListener("click", () => console.log("button 2"));
 labelThreeBtn.addEventListener("click", () => console.log("button 3"));
 
+nomaskbtn.addEventListener("click", () => addNoMask())
+maskbtn.addEventListener("click", () => addMask())
+trainbtn.addEventListener("click", () => train())
 trainbtn.addEventListener("click", () => console.log("train"));
 
 if (navigator.mediaDevices.getUserMedia) {
@@ -21,6 +29,47 @@ if (navigator.mediaDevices.getUserMedia) {
         .catch((err) => {
             console.log("Something went wrong!");
         });
+}
+
+function modelLoaded(){
+    console.log("The mobileNet model is loaded!")
+    classifier = featureExtractor.classification(video, videoReady)
+}
+
+function videoReady(){
+    console.log(classifier)
+}
+
+function addNoMask(){
+    classifier.addImage(video, "draagt geen masker", addedImage)
+}
+
+function addMask() {
+    classifier.addImage(video, "draagt masker", addedImage)
+}
+
+function train(){
+    console.log("start training...")
+    classifier.train((lossValue) => {
+        console.log(lossValue)
+        if(lossValue == null){
+            startClassifying()
+        }
+    })
+}
+
+function startClassifying(){
+    setInterval(()=>{
+        classifier.classify(video, (err, result)=>{
+            if(err) console.log(err)
+            console.log(result)
+            label.innerHTML = result[0].label
+        })
+    }, 1000)
+}
+
+function addedImage(){
+    console.log("added image to network")
 }
 
 label.innerText = "Ready when you are!";
