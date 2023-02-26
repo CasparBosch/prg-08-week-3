81940252bee98cfd7da48790ff7413bde45a8c99
CasparBosch <1004288@hr.nl>
2023-02-26 20:28:16 +0100

    comments

--- js/app.js
+++ js/app.js
@@ -1,33 +1,14 @@
+//definitions
 const video = document.getElementById("webcam");
-const featureExtractor = ml5.featureExtractor('MobileNet', modelLoaded)
 const label = document.getElementById("label");
-let classifier
-let synth = window.speechSynthesis
-let scores = 0
 const score = document.getElementById("score");
-
-//query selectors
-// const labelOneBtn = document.querySelector("#labelOne");
-// const labelTwoBtn = document.querySelector("#labelTwo");
-// const trainbtn = document.querySelector("#train");
-// const savebtn = document.querySelector("#save");
-// const btn = document.querySelector("#speak");
+const featureExtractor = ml5.featureExtractor('MobileNet',{numLabels : 3}, modelLoaded)
 const image = document.getElementById('output')
 const fileButton = document.querySelector("#file")
+let scores = 0
 
-//eventlistners
-// labelOneBtn.addEventListener("click", () => addMotor(), console.log("button 1"));
-// labelTwoBtn.addEventListener("click", () => addAuto(), console.log("button 2"));
-// trainbtn.addEventListener("click", () => train(), console.log("train"));
-// savebtn.addEventListener("click", () => save());
-// btn.addEventListener("click", () => {
-//     speak('')
-//   })
-fileButton.addEventListener("change", (event)=>{
-    image.src = URL.createObjectURL(event.target.files[0])
-})
-image.addEventListener('load', () => userImageUploaded())
-
+//text to speech
+let synth = window.speechSynthesis
 function speak(text) {
     if (synth.speaking) {
         console.log('still speaking...')
@@ -39,84 +20,46 @@ function speak(text) {
     }
 }
 
-if (navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices
-        .getUserMedia({ video: true })
-        .then((stream) => {
-            video.srcObject = stream;
-        })
-        .catch((err) => {
-            console.log("Something went wrong!");
-        });
+//file upload
+fileButton.addEventListener("change", (event)=>{
+    image.src = URL.createObjectURL(event.target.files[0])
+
+})
+
+image.addEventListener('load', () => userImageUploaded())
+
+label.innerText = "Loading Model";
+
+//loading model and custom model
+function modelLoaded() {
+    console.log('Model Loaded!')
+    classifier = featureExtractor.classification(image, videoReady)
+    classifier.load('model.json', customModelReady)
 }
 
-label.innerText = "Ready when you are!";
-
-function modelLoaded(){
-    console.log("The mobileNet model is loaded!")
-    classifier = featureExtractor.classification(video, videoReady)
-    classifier.load('model.json', customModelReady());
-}
-
+//custom model
 function customModelReady(){
-    console.log('Custom Model is ready!');
-    label.innerText = 'Model is Ready!'
+    console.log("Custom Model Is Ready")
+    label.innerText = "Model is Ready"
 }
 
+//video ready
 function videoReady(){
-    console.log(classifier)
+    console.log("the webcam is ready")
 }
 
-// function addMotor(){
-//     classifier.addImage(video, "Motor", ()=>{
-//         console.log("added image to model!")
-//     })
-// }
-
-// function addAuto(){
-//     classifier.addImage(video, "Auto", ()=>{
-//         console.log("added image to model!")
-//     })
-// }
-
-function train(){
-    console.log("start training...")
-    classifier.train((lossValue) => {
-        console.log(lossValue)
-        if(lossValue == null){
-            userImageUploaded()
-        }
-    })
-}
-
-function save(){
-    classifier.save();
-
-}
-
-// function startClassifying(){
-//     setInterval(()=>{
-//         classifier.classify(video, (err, result)=>{
-//             if(err) console.log(err)
-//             console.log(result)
-//             label.innerHTML = result[0].label
-//         })
-//     }, 1000)
-// }
-
+//result
 function userImageUploaded(){
     console.log("The image is now visible in the DOM")
     intervalId = setInterval(()=>{
         classifier.classify(image, (err, result) => {
             if (err) console.log(err)
             console.log(result)
-            // label.innerText = result[0].label
-            console.log(label.innerText = result[0].label)
-            label.innerText = `This is probably a ${result[0].label} (I'm ${Math.round(result[0].confidence * 100)}% confident) or a ${result[1].label}`;
-            speak(`This is probably a ${result[0].label} (I'm ${Math.round(result[0].confidence * 100)}% confident) or a ${result[1].label}`) 
-            scores++
+            label.innerText = `This is probably a ${result[0].label} (I'm ${Math.round(result[0].confidence * 100)}% confident) or a ${result[1].label} (I'm ${Math.round(result[1].confidence * 100)}% confident)`;
+            speak(`This is probably a ${result[0].label} (I'm ${Math.round(result[0].confidence * 100)}% confident) or a ${result[1].label} (I'm ${Math.round(result[1].confidence * 100)}% confident)`)
+            scores ++
             score.innerText = "Score = " + scores
-            console.log(scores)   
+            console.log(scores)
             
         })
     }, 20)
@@ -125,10 +68,106 @@ function userImageUploaded(){
       }, 21)
 }   
 
-function addedImage(){
-    console.log("added image to network")
-}
 
-function userImageUploaded(){
-    console.log("The image is now visible in the DOM")
-}
+//commented out
+
+// const labelOneBtn = document.querySelector("#labelOne");
+// const labelTwoBtn = document.querySelector("#labelTwo");
+// const labelThreeBtn = document.querySelector("#labelThree");
+// const trainbtn = document.querySelector("#train");
+// const savebtn = document.querySelector("#save");
+
+// labelOneBtn.addEventListener("click", () => addmotorImage());
+// labelTwoBtn.addEventListener("click", () => addAutoImage());
+// trainbtn.addEventListener("click", () => train());
+// savebtn.addEventListener("click", () => save());
+
+// if (navigator.mediaDevices.getUserMedia) {
+//     navigator.mediaDevices
+//         .getUserMedia({ video: true })
+//         .then((stream) => {
+//             video.srcObject = stream;
+//         })
+//         .catch((err) => {
+//             console.log("Something went wrong!");
+//         });
+// }
+
+//add motor image to model
+// function addMotorImage() {
+//     classifier.addImage(video, 'Motor', ()=>{
+//         console.log("added image to model!")
+//     })
+// }
+
+//add auto image to model
+// function addAutoImage() {
+//     classifier.addImage(video, 'Auto', ()=>{
+//         console.log("added image to model!")
+//     })
+// }
+
+//train model
+// function train(){
+//     console.log("start training...")
+//     classifier.train((lossValue) => {
+//         console.log('Loss is', lossValue)
+//         if(lossValue == null) console.log("Finished training")
+//     })
+// }
+
+//save 
+// function save() {
+//     classifier.save();
+// }
+
+
+/*
+const URL = "./my_model/";
+
+    let model, webcam, labelContainer, maxPredictions;
+
+    // Load the image model and setup the webcam
+    async function init() {
+        const modelURL = URL + "model.json";
+        const metadataURL = URL + "metadata.json";
+
+        // load the model and metadata
+        // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
+        // or files from your local hard drive
+        // Note: the pose library adds "tmImage" object to your window (window.tmImage)
+        model = await tmImage.load(modelURL, metadataURL);
+        maxPredictions = model.getTotalClasses();
+
+        // Convenience function to setup a webcam
+        const flip = true; // whether to flip the webcam
+        webcam = new tmImage.Webcam(200, 200, flip); // width, height, flip
+        await webcam.setup(); // request access to the webcam
+        await webcam.play();
+        window.requestAnimationFrame(loop);
+
+        // append elements to the DOM
+        document.getElementById("webcam-container").appendChild(webcam.canvas);
+        labelContainer = document.getElementById("label-container");
+        for (let i = 0; i < maxPredictions; i++) { // and class labels
+            labelContainer.appendChild(document.createElement("div"));
+        }
+    }
+
+    async function loop() {
+        webcam.update(); // update the webcam frame
+        await predict();
+        window.requestAnimationFrame(loop);
+    }
+
+    // run the webcam image through the image model
+    async function predict() {
+        // predict can take in an image, video or canvas html element
+        const prediction = await model.predict(webcam.canvas);
+        for (let i = 0; i < maxPredictions; i++) {
+            const classPrediction =
+                prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+            labelContainer.childNodes[i].innerHTML = classPrediction;
+        }
+    } 
+    */      
